cf2d594d3d12290b6ebeb10011c5df7b6414f545
noppakrit55555 <48791557+noppakrit55555@users.noreply.github.com>
2020-09-17 17:42:06 +0700

    Create m200917_095518_create_post_table.php

---
+++ console/migrations/m200917_095518_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m200917_095518_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string(250)->notNull(),
+            'short description'=>$this->string(400),
+            'description'=>$this->text(),
+            'is_active'=>$this->boolean()->defaultValue(0),
+            'create_at'=>$this->integer(),
+            'create_by'=>$this->integer(),
+            'update_at'=>$this->integer(),
+            'update_by'=>$this->integer()
+
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post}}');
+    }
+}
